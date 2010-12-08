271db3bb3df218fa0e703d78ec9f00e1f66d3e44
Simon Charvet <charvet@lii-enac.fr>
2010-12-08 16:05:11 +0100

    Stop sending ABS_X and ABS_Y.
    This patch deletes the mouse emulation code that was useless.
    The bridge now sends only multitouch events. It may not be recognized
    by the xserver now as an input absolute device.
    
    Signed-off-by: Simon Charvet <charvet@lii-enac.fr>

--- passerelle.py
+++ passerelle.py
@@ -14,19 +14,16 @@ class Device:
         self.empty=1
         capabilities= {
         uinput.EV_KEY: [uinput.BTN_TOUCH],
-        uinput.EV_ABS: [uinput.ABS_MT_POSITION_X, uinput.ABS_MT_POSITION_Y,uinput.ABS_MT_TRACKING_ID, uinput.ABS_X, uinput.ABS_Y],
+        uinput.EV_ABS: [uinput.ABS_MT_POSITION_X, uinput.ABS_MT_POSITION_Y,uinput.ABS_MT_TRACKING_ID],
         }
         abs_parameters = {uinput.ABS_MT_POSITION_X:(0, 1000, 0, 0), uinput.ABS_MT_POSITION_Y:(0, 1000, 0, 0),
-        uinput.ABS_MT_TRACKING_ID:(0, 10, 0, 0), uinput.ABS_X:(0,1000,0,0), uinput.ABS_Y:(0,1000,0,0)}
+        uinput.ABS_MT_TRACKING_ID:(0, 10, 0, 0)}
         self.device=uinput.Device(name="TUIO-multitouch")
         self.device.capabilities=capabilities
         self.device.abs_parameters=abs_parameters
         self.x_mouse=0
         self.y_mouse=0
-        self.x=[]
-        self.y=[]
         self.cpt=0
-        self.press=0
 
     def update(self,list_obj,list_cursors):
         list=set(list_obj).union(set(list_cursors))
@@ -51,44 +48,6 @@ class Device:
             self.device.emit(0, 2, 0, syn=False)
             print("type :%d code : %d value : %d"%(0,2,0))
             i+=1
-            self.x.append(obj.xpos*1000)
-            self.y.append(obj.ypos*1000)
-            if (max(self.x)-min(self.x)<10)&(max(self.y)-min(self.y)<10):
-                self.cpt+=1
-                self.rectangle=1
-                print self.cpt
-            else:
-                self.cpt=0
-                self.rectangle=0
-                self.x=[]
-                self.y=[]
-            if self.rectangle & (self.cpt>200):
-                if self.press:
-                    self.device.emit(uinput.EV_KEY, uinput.BTN_TOUCH, 0)
-                    print ("type :%d code : %d value : %d" % (uinput.EV_KEY, uinput.BTN_TOUCH, 0))
-                    self.press=0
-                else:
-                    self.device.emit(uinput.EV_KEY, uinput.BTN_TOUCH, 1)
-                    print ("type :%d code : %d value : %d" % (uinput.EV_KEY, uinput.BTN_TOUCH, 1))
-                    self.press=1
-                self.x=[]
-                self.y=[]
-                self.cpt=0
-                self.rectangle=0
-            if self.empty:
-                if self.x_mouse!=obj.xpos*1000:
-                    self.x_mouse=obj.xpos*1000
-                    self.device.emit(uinput.EV_ABS, uinput.ABS_X, self.x_mouse,syn=False)
-                    print ("type :3 code :0 value :%d"%self.x_mouse)
-                if self.y_mouse!=obj.ypos*1000:
-                    self.y_mouse=obj.ypos*1000
-                    self.device.emit(uinput.EV_ABS, uinput.ABS_Y, self.y_mouse,syn=False)
-                    print ("type :3 code :1 value :%d"%self.y_mouse)
-                self.empty=0
-            else:
-                print "reset"
-                self.cpt=0
-                self.empty=0
             if i==len(self.objects):
                 self.device.emit(0,0,0,syn=False)
                 print ("type :0 code :0 value :0")
