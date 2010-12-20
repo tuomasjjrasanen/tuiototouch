22332538f684a02ae4b458af9c805c4de940db45
Simon Charvet <charvet@lii-enac.fr>
2010-12-20 17:24:05 +0100

    New class implementation including mouse emulation
    Separation of the code between two classes to choose
    at the application launch if the mouse emulation is
    wanted or not with the option '--no-mouse-emu'.
    
    Signed-off-by: Simon Charvet <kandrath@gmail.com>

--- tuiototouch.py
+++ tuiototouch.py
@@ -58,9 +58,60 @@ class Device(object):
             self.device.emit(0, 2, 0, syn=False)
             print("type :%d code : %d value : %d"%(0,2,0))
 
+class DeviceWME(Device):
+
+    def __init__(self):
+        Device.__init__(self)
+        self.capabilities[uinput.EV_ABS] = \
+            [uinput.ABS_MT_POSITION_X, uinput.ABS_MT_POSITION_Y,
+            uinput.ABS_MT_TRACKING_ID,
+            uinput.ABS_X, uinput.ABS_Y]
+        self.abs_parameters[uinput.ABS_X] = (0,1000,0,0)
+        self.abs_parameters[uinput.ABS_Y] = (0,1000,0,0)
+        self.device.capabilities=self.capabilities
+        self.device.abs_parameters=self.abs_parameters
+
+    def display(self):
+        if (len(self.objects)==0) & self.empty:
+            print ("type :%d code : %d value : %d" % (uinput.EV_KEY, uinput.BTN_TOUCH, 0))
+            self.device.emit(uinput.EV_KEY, uinput.BTN_TOUCH, 0)
+            self.empty=0
+        i=0
+        for key in self.objects:
+            if not self.empty:
+                print ("type :%d code : %d value : %d" % (uinput.EV_KEY, uinput.BTN_TOUCH, 1))
+                self.device.emit(uinput.EV_KEY, uinput.BTN_TOUCH, 1)
+            self.empty=1
+            obj = self.objects[key]
+            if i==0:
+                modified=0
+                if self.x_mouse!=obj.xpos*1000:
+                    modified=1
+                    self.x_mouse=obj.xpos*1000
+                    print ("type :3 code :0 value :%d"%self.x_mouse)
+                    self.device.emit(uinput.EV_ABS, uinput.ABS_X, self.x_mouse, syn=False)
+                if self.y_mouse!=obj.ypos*1000:
+                    modified=1
+                    self.y_mouse=obj.ypos*1000
+                    print ("type :3 code :1 value :%d"%self.y_mouse)
+                    self.device.emit(uinput.EV_ABS, uinput.ABS_Y, self.y_mouse, syn=False)
+                if modified:
+                    print ("type: 0 code :0 value :0")
+                    self.device.emit(0,0,0,syn=False)
+            self.treatment(obj)
+            i+=1
+            if i==len(self.objects):
+                self.device.emit(0,0,0,syn=False)
+                print ("type :0 code :0 value :0")
+                self.empty=1
+
 if __name__ == "__main__":
+    import sys
     tracking=tuio.Tracking()
-    device=Device()
+    if '--no-mouse-emu' in sys.argv:
+        device=Device()
+    else:
+        device=DeviceWME()
     try:
         while 1:
             tracking.update()
